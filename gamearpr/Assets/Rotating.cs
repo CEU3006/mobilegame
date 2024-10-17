cfebd8b21a4b8a247656fdbded78a158ace074f2
CEU3006 <114677174+CEU3006@users.noreply.github.com>
2024-10-17 14:23:42 +0100

    actual game

---
+++ gamearpr/Assets/Rotating.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotating : MonoBehaviour
+{
+    private Quaternion correctQuaternion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Input.gyro.enabled = true;
+        Debug.Log("Gyro Enabled");
+        correctQuaternion = Quaternion.Euler(90f, 0f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = correctQuaternion * GyrotoUnity(Input.gyro.attitude);
+    }
+    private Quaternion GyrotoUnity(Quaternion q)
+    {
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
+    }
+}
